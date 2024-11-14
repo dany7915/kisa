049cdfd88feebd7b221d738026363986ec7af423
Daniel <dany7915@gmail.com>
2024-11-14 11:44:23 +0900

    mdps12

--- opendbc_repo/opendbc/car/hyundai/hyundaican.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaican.py
@@ -333,3 +333,16 @@ def create_scc42a(packer):
     "CF_FCA_Equip_Front_Radar": 1
   }
   return packer.make_can_msg("FRT_RADAR11", 0, values)
+
+def create_mdps12(packer, frame, mdps12):
+  values = mdps12
+  values["CF_Mdps_ToiActive"] = 0
+  values["CF_Mdps_ToiUnavail"] = 1
+  values["CF_Mdps_MsgCount2"] = frame % 0x100
+  values["CF_Mdps_Chksum2"] = 0
+
+  dat = packer.make_can_msg("MDPS12", 2, values)[1]
+  checksum = sum(dat) % 256
+  values["CF_Mdps_Chksum2"] = checksum
+
+  return packer.make_can_msg("MDPS12", 2, values)
